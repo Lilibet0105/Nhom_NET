0311f934ffdade79cf0139658b4a32407f19fc5b
Twe <xuantue4a1@gmail.com>
2026-05-28 21:03:11 +0700

    sua lai may cònlict

---
+++ [.Net] - PCCV/[.Net] - PCCV/GUI/CalendarView.Designer.cs
@@ -0,0 +1,310 @@
+namespace _.Net____PCCV
+{
+    partial class CalendarView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			//
+			// button1
+			//
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(34, 533);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(155, 36);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Thêm Lịch";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			//
+			// button2
+			//
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(336, 533);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(155, 36);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Sửa Lịch";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			//
+			// button3
+			//
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(644, 533);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(155, 36);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Cập Nhật Lịch";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			//
+			// button4
+			//
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(946, 533);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(155, 36);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Xóa Lịch";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			//
+			// dateTimePicker1
+			//
+			this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.dateTimePicker1.Location = new System.Drawing.Point(754, 100);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(347, 30);
+			this.dateTimePicker1.TabIndex = 4;
+			//
+			// dateTimePicker2
+			//
+			this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.dateTimePicker2.Location = new System.Drawing.Point(754, 161);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(347, 30);
+			this.dateTimePicker2.TabIndex = 5;
+			//
+			// dataGridView1
+			//
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(48, 359);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(1031, 146);
+			this.dataGridView1.TabIndex = 6;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			//
+			// textBox1
+			//
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(162, 79);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(281, 30);
+			this.textBox1.TabIndex = 7;
+			//
+			// textBox2
+			//
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(162, 137);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(281, 30);
+			this.textBox2.TabIndex = 8;
+			//
+			// textBox3
+			//
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(162, 193);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(281, 30);
+			this.textBox3.TabIndex = 9;
+			//
+			// textBox4
+			//
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(162, 252);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(281, 87);
+			this.textBox4.TabIndex = 10;
+			//
+			// comboBox1
+			//
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Chưa Hoàn Thành",
+            "Đang Tiến Hành",
+            "Hoàn Thành",
+            "Chậm Tiến Độ"});
+			this.comboBox1.Location = new System.Drawing.Point(754, 218);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(347, 33);
+			this.comboBox1.TabIndex = 11;
+			//
+			// label1
+			//
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label1.Location = new System.Drawing.Point(394, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(265, 38);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "LỊCH LÀM VIỆC";
+			//
+			// label2
+			//
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(29, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(81, 25);
+			this.label2.TabIndex = 13;
+			this.label2.Text = "Mã Lịch";
+			//
+			// label3
+			//
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(29, 140);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(81, 25);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "Tiêu Đề";
+			//
+			// label4
+			//
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(19, 196);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(91, 25);
+			this.label4.TabIndex = 15;
+			this.label4.Text = "Địa Điểm";
+			//
+			// label5
+			//
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(29, 252);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(69, 25);
+			this.label5.TabIndex = 16;
+			this.label5.Text = "Mô Tả";
+			//
+			// label6
+			//
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(538, 105);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(172, 25);
+			this.label6.TabIndex = 17;
+			this.label6.Text = "Thời Gian Bắt Đầu";
+			//
+			// label7
+			//
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(528, 166);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(182, 25);
+			this.label7.TabIndex = 18;
+			this.label7.Text = "Thời Gian Kết Thúc";
+			//
+			// label8
+			//
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(602, 221);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(108, 25);
+			this.label8.TabIndex = 19;
+			this.label8.Text = "Trạng Thái";
+			//
+			// CalendarView
+			//
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1126, 581);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.dateTimePicker2);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "CalendarView";
+			this.Text = "CalendarView";
+			this.Load += new System.EventHandler(this.CalendarView_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
--- [.Net] - PCCV/[.Net] - PCCV/GUI/frmMainDashboard.cs
+++ [.Net] - PCCV/[.Net] - PCCV/GUI/frmMainDashboard.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -63,7 +63,7 @@ namespace GUI
 
         private void btnCalendar_Click(object sender, EventArgs e)
         {
-            OpenChildForm(new frmCalendarView());
+            OpenChildForm(new _.Net____PCCV.CalendarView());
         }
 
         private void btnReport_Click(object sender, EventArgs e)
